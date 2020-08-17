57f2d6c18c1b7b90445751043e608659a2e51a92
David van der Leij <davidvanderleij@gmail.com>
2020-08-17 15:32:41 +0200

    Allow for HikariCP, mariadb-specific-driver, and postgresql connection.

--- src/main/java/refactoringml/db/HibernateConfig.java
+++ src/main/java/refactoringml/db/HibernateConfig.java
@@ -1,89 +1,175 @@
 package refactoringml.db;
 
-import org.hibernate.SessionFactory;
-import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
-import org.hibernate.cfg.Configuration;
-import org.hibernate.cfg.Environment;
-import org.hibernate.service.ServiceRegistry;
-
-import javax.persistence.CascadeType;
-import javax.persistence.ManyToOne;
+import java.util.Optional;
 import java.util.Properties;
 
-public class HibernateConfig {
+import com.zaxxer.hikari.HikariDataSource;
 
-	private static SessionFactory sessionFactory;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.AvailableSettings;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
 
-	public SessionFactory getSessionFactory(String url, String user, String pwd) {
+public abstract class HibernateConfig {
+
+	protected final Configuration configuration = new Configuration();
+
+	public static SessionFactory getSessionFactory(String url, String user, String pwd) {
 		return getSessionFactory(url, user, pwd, false);
 	}
 
-	public SessionFactory getSessionFactory(String url, String user, String pwd, boolean drop) {
-		if(sessionFactory == null) {
-			Configuration configuration = new Configuration();
+	public static SessionFactory getSessionFactory(String url, String user, String pwd, boolean drop) {
+		HibernateConfig conf = new C3p0HibernateConfig();
+		conf.setDatabaseEngineSpecificProperties(DatabaseEngine.MYSQL.driverClass, url,
+				DatabaseEngine.MYSQL.dialectClass);
+		return conf.buildSessionFactory(user, pwd, drop ? "create-drop" : "update");
+	}
 
-			Properties settings = new Properties();
-			settings.put(Environment.DRIVER, "com.mysql.cj.jdbc.Driver");
-			settings.put(Environment.URL, url);
-			settings.put(Environment.USER, user);
-			settings.put(Environment.PASS, pwd);
-			settings.put(Environment.DIALECT, "org.hibernate.dialect.MySQL5InnoDBDialect");
-			settings.put(Environment.SHOW_SQL, "false");
+	public static SessionFactory getSessionFactory(String host, Optional<Short> port, String databaseName, String user,
+			String pwd, String hbm2ddlStrategy, DatabaseEngine dbEngine, ConnectionPoolType cpType) {
 
-			if(drop)
-				settings.put(Environment.HBM2DDL_AUTO, "create-drop");
-			else
-				settings.put(Environment.HBM2DDL_AUTO, "update");
+		HibernateConfig conf = cpType.toHibernateConfig();
+		String url = String.format("jdbc:%s://%s:%d/%s", dbEngine.protocol, host, port.orElse(dbEngine.defaultPort), databaseName);
+
+		conf.setDatabaseEngineSpecificProperties(dbEngine.driverClass, url, dbEngine.dialectClass);
+		return conf.buildSessionFactory(user, pwd, hbm2ddlStrategy);
+	}
+
+	protected abstract void setCommonProperties(String user, String pwd);
+
+	protected abstract void setDatabaseEngineSpecificProperties(String driverClassName, String url,
+			String dialectClassName);
+
+	protected abstract ServiceRegistry buildServiceRegistry();
+
+	private SessionFactory buildSessionFactory(String user, String pwd, String hbm2ddlStrategy) {
+		setCommonProperties(user, pwd);
+		configuration.setProperty(AvailableSettings.HBM2DDL_AUTO, hbm2ddlStrategy);
+
+		configuration.addAnnotatedClass(RefactoringCommit.class);
+		configuration.addAnnotatedClass(StableCommit.class);
+		configuration.addAnnotatedClass(Project.class);
+
+		// features of Instance for DB normalization
+		configuration.addAnnotatedClass(CommitMetaData.class);
+		configuration.addAnnotatedClass(ClassMetric.class);
+		configuration.addAnnotatedClass(MethodMetric.class);
+		configuration.addAnnotatedClass(VariableMetric.class);
+		configuration.addAnnotatedClass(FieldMetric.class);
+		configuration.addAnnotatedClass(ProcessMetrics.class);
+
+		return configuration.buildSessionFactory(buildServiceRegistry());
+	}
+
+	private static class HikariHibernateConfig extends HibernateConfig {
+
+		private final HikariDataSource ds = new HikariDataSource();
+
+		@Override
+		protected void setCommonProperties(String user, String pwd) {
+			ds.setMaximumPoolSize(Runtime.getRuntime().availableProcessors() + 4);
+			ds.addDataSourceProperty("user", user);
+			ds.addDataSourceProperty("password", pwd);
+			ds.setAutoCommit(false);
+		}
+
+		@Override
+		protected void setDatabaseEngineSpecificProperties(String driverClassName, String url,
+				String dialectClassName) {
+			ds.setDriverClassName(driverClassName);
+			ds.setJdbcUrl(url);
+			configuration.setProperty(AvailableSettings.DIALECT, dialectClassName);
+		}
+
+		@Override
+		protected ServiceRegistry buildServiceRegistry() {
+			return new StandardServiceRegistryBuilder().applySettings(configuration.getProperties())
+					.applySetting(AvailableSettings.DATASOURCE, ds).build();
+		}
+
+	}
+
+	private static class C3p0HibernateConfig extends HibernateConfig {
+
+		private final Properties settings = new Properties();
+
+		@Override
+		protected void setCommonProperties(String user, String pwd) {
+
+			settings.put(AvailableSettings.USER, user);
+			settings.put(AvailableSettings.PASS, pwd);
 
 			settings.put("hibernate.connection.provider_class", "org.hibernate.connection.C3P0ConnectionProvider");
 			settings.put("hibernate.c3p0.acquire_increment", 1);
+			settings.put("hibernate.c3p0.idle_test_period", 60);
 			settings.put("hibernate.c3p0.min_size", 1);
 			settings.put("hibernate.c3p0.max_size", 5);
-			settings.put("hibernate.c3p0.max_statements", 100);
+			settings.put("hibernate.c3p0.max_statements", 50);
 
-			settings.put("hibernate.c3p0.numHelperThreads", 10);
+			settings.put("hibernate.c3p0.timeout", 300);
+			settings.put("hibernate.c3p0.maxConnectionAge", 3600);
 
-			// this one should be here only for debug purposes
-			// settings.put("hibernate.c3p0.debugUnreturnedConnectionStackTraces", true);
-			// settings.put("hibernate.c3p0.unreturnedConnectionTimeout", 120);
-
-			settings.put("hibernate.c3p0.timeout", 0);
-			settings.put("hibernate.c3p0.maxConnectionAge", 0);
-
-			settings.put("hibernate.c3p0.maxIdleTimeExcessConnections", 0);
+			settings.put("hibernate.c3p0.maxIdleTimeExcessConnections", 3600);
 
 			settings.put("hibernate.c3p0.acquireRetryAttempts", 10);
-			settings.put("hibernate.c3p0.acquireRetryDelay", 5);
+			settings.put("hibernate.c3p0.acquireRetryDelay", 60);
+
+			settings.put("hibernate.c3p0.preferredTestQuery", "SELECT 1");
+			settings.put("hibernate.c3p0.testConnectionOnCheckout", true);
 
 			settings.put("hibernate.c3p0.autoCommitOnClose", false);
-
-			// testing connections on checkout is more reliable, but also
-			// less performant: https://www.mchange.com/projects/c3p0/#idleConnectionTestPeriod
-			// for now, it's ok, as we don't really need high performance...
-			settings.put("hibernate.c3p0.testConnectionOnCheckout", true);
-			settings.put("hibernate.c3p0.idle_test_period", 120);settings.put("hibernate.c3p0.idle_test_period", 120);
-			settings.put("hibernate.c3p0.preferredTestQuery", "SELECT 1");
+			settings.put("hibernate.c3p0.unreturnedConnectionTimeout", 300);
 
 			configuration.setProperties(settings);
-
-			configuration.addAnnotatedClass(RefactoringCommit.class);
-			configuration.addAnnotatedClass(StableCommit.class);
-			configuration.addAnnotatedClass(Project.class);
-
-			//features of Instance for DB normalization
-			configuration.addAnnotatedClass(CommitMetaData.class);
-			configuration.addAnnotatedClass(ClassMetric.class);
-			configuration.addAnnotatedClass(MethodMetric.class);
-			configuration.addAnnotatedClass(VariableMetric.class);
-			configuration.addAnnotatedClass(FieldMetric.class);
-			configuration.addAnnotatedClass(ProcessMetrics.class);
-
-			ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
-					.applySettings(configuration.getProperties()).build();
-
-			sessionFactory = configuration.buildSessionFactory(serviceRegistry);
 		}
 
-		return sessionFactory;
+		@Override
+		protected void setDatabaseEngineSpecificProperties(String driverClassName, String url,
+				String dialectClassName) {
+			settings.put(AvailableSettings.DRIVER, driverClassName);
+			settings.put(AvailableSettings.URL, url);
+
+			settings.put(AvailableSettings.DIALECT, dialectClassName);
+			settings.put(AvailableSettings.SHOW_SQL, "false");
+		}
+
+		@Override
+		protected ServiceRegistry buildServiceRegistry() {
+			return new StandardServiceRegistryBuilder().applySettings(configuration.getProperties()).build();
+		}
+
+	}
+
+	public enum DatabaseEngine {
+		MYSQL("mysql", "com.mysql.cj.jdbc.Driver", "org.hibernate.dialect.MySQL8Dialect", (short) 3306),
+		MARIADB("mariadb", "org.mariadb.jdbc.Driver", "org.hibernate.dialect.MariaDBDialect", (short) 3306),
+		POSTGRESQL("postgresql", "org.postgresql.Driver", "org.hibernate.dialect.PostgreSQL95Dialect", (short) 5432);
+
+		private final String protocol;
+		private final String driverClass;
+		private final String dialectClass;
+		private final short defaultPort;
+
+		private DatabaseEngine(String protocol, String driverClass, String dialectClass, short defaultPort) {
+			this.protocol = protocol;
+			this.driverClass = driverClass;
+			this.dialectClass = dialectClass;
+			this.defaultPort = defaultPort;
+		}
+	}
+
+	public enum ConnectionPoolType {
+		C3P0, HIKARICP;
+
+		public HibernateConfig toHibernateConfig() {
+			switch (this) {
+				case C3P0:
+					return new C3p0HibernateConfig();
+				case HIKARICP:
+					return new HikariHibernateConfig();
+			}
+			throw new IllegalArgumentException("Not a supported ConnectionPoolType type: " + toString());
+		}
 	}
 }
